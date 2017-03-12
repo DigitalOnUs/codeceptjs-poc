cff0b8b353dbb6a94641f2b6f9d5832d11fb4c75
Alan Trevino <atrevino1989@gmail.com>
2017-03-12 14:02:46 -0600

    Adding initial structure for codeceptjs

---
+++ restful_test.js
@@ -0,0 +1,13 @@
+/// <reference path="./steps.d.ts" />
+
+Feature('REST helper');
+
+Scenario('Testing one Get call', (I) => {
+  I.
+});
+
+Scenario('Testing multipple Get calls', (I) => {
+    /**
+     * TODO: Create a REST TEST Scenario 2: Multiple gets
+     */
+});
