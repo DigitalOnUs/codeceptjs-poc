19f11a824cd49940c6b665b75acd96537f693289
Alan Trevino <atrevino1989@gmail.com>
2017-03-12 21:16:33 -0600

    Adding REST test scenarios.

--- tests/restful_test.js
+++ tests/restful_test.js
@@ -1,14 +1,15 @@
-/// <reference path="./steps.d.ts" />
-
 Feature('REST http test');
 
-Scenario('I can make multiple GET requests and collect responses', function* (I) {
-
+Scenario('I can do GET requests', function*(I) {
     let randomApiResponse = yield I.sendGet('https://randomuser.me/api/')
-    // I.say(`Status code from the call: ${randomApiResponse.statusCode}`)
-    // I.say(`Raw response:\n ${randomApiResponse.raw_body}`)
-    // I.say(`Selecting one item from the json response of this call:\n Gender: ${JSON.stringify(randomApiResponse.body.results[0].gender)}`)
-    //
-    // let postResponse = yield I.sendPost('https://jsonplaceholder.typicode.com/posts', payload={id: 101, title: 'foo', body: 'bar', userId: 1})
-    // I.say(`response from ${JSON.stringify(postResponse)}`)
+
+    I.say(`Status code from the call: ${randomApiResponse.statusCode}`)
+    I.say(`Raw response:\n ${randomApiResponse.raw_body}`)
+    I.say(`Selecting one item from the json response of this call:\n Gender: ${JSON.stringify(randomApiResponse.body.results[0].gender)}`)
+})
+
+Scenario('I can do a POST call', function*(I) {
+    let response = yield I.sendPost('http://httpbin.org/post')
+
+    I.say(`Response from ${response.raw_body}`)
 })
